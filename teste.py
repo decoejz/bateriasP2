17bd3993bfefe11f46c040b7222e3b270a8d4454
decoejz <decoejz@gmail.com>
2019-04-16 15:58:57 -0300

    funcionando o calculo de capacidade de carga

---
+++ teste.py
@@ -0,0 +1,6 @@
+import utils
+
+mat1 = {"eletron":2,"massa":65}
+mat2 = {"eletron":1,"massa":63.6}
+
+utils.calcula_cap(mat1,mat2,65,100)
--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 class pilha():
-    def __init__(self,mat1,mat2):
+    def __init__(self,mat1,mat2,massa1,massa2):
+        self.mat1 = mat1
+        self.mat2 = mat2
         self.ddp = calcula_DDP(mat1,mat2)
-        self.cap_carga = calcula_cap(mat1,mat2)
+        self.cap_carga = calcula_cap(mat1,mat2,massa1,massa2)
         self.potencia = calcula_potencia(mat1,mat2)
         self.tempo_ligado = calcula_tempo(mat1,mat2)
 
@@ -15,8 +17,23 @@ def calcula_DDP(mat1,mat2):
     ddp = maior - mat2.E
     return ddp
 
-def calcula_cap(mat1,mat2):
-    Ah = carga * (1.93 * (10**5)) / 3600
+def calcula_cap(mat1,mat2,massa1,massa2):
+    c_eletron = 1.6021 * (10 ** (-19))
+    mol = 6.0225 * (10 ** (23))
+    F = c_eletron * mol
+
+    limitante = massa1
+    material = mat1
+    contra_mat = mat2
+    if (massa2 < limitante):
+        limitante = massa2
+        material = mat2
+        contra_mat = mat1
+
+    total_mol_e = (material["eletron"] * limitante * contra_mat["eletron"]) / (material["massa"])
+
+    Ah = F * total_mol_e / 3600
+    #print(Ah)
     return Ah
 
 def calcula_potencia():
