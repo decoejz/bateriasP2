e1d6d57de4b6ab9f8251e8b80db568e6ad4c3799
unknown <manucirne@gmail.com>
2019-04-20 22:16:42 -0300

    Precisa conferir, a parte 2 está com um bug, talvez seja bom refazer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import utils as ut
+from utils import pilha
+from utils import Bateria_Escolhida
 import sys
 import json
 
@@ -7,51 +8,96 @@ def show_json(dic):
         print(element)
         print(dic[element])
 
-def etapa1(dic): #Seleção de material para cátodo e anôdo
+def menu_principal(dic): #Seleção de material para cátodo e anôdo
         print("--------------------------------------------------------------------------------------------")
-        print("ETAPA 1: SELEÇÃO DE MATERIAIS PARA A PILHA")
-        print("Comandos:")
-        print("Show             -------------------       Mostra os metais disponíveis e suas semirreações")
-        print("Selecionar       -------------------       Selecione o número correspondente ao metal")
-        print("Sair")
+        print("Digite C para construir sua própria bateria com os materiais disponíveis.")
+        print("Digite S para selecionar uma pilha comercial que sirva para o seu experimento")
+        print("Depos siga as instruções para obtenção da informação desejada")
         print("--------------------------------------------------------------------------------------------")
         while(True):
-            comando = input("Digite o comando: ")
-            if (comando == "Selecionar") or (comando == "selecionar"):
-                mat_catodo_id = int(input("Qual o id do metal para o cátodo? "))
-                massa_catodo = int(input("Qual é a massa (em g) do metal para o cátodo? "))
-                mat_anodo_id = int(input("Qual o id do metal para o ânodo? "))
-                massa_anodo = int(input("Qual é a massa (em g) do metal para o ânodo? "))
-                mat_catodo = dic[mat_catodo_id]
-                mat_anodo = dic[mat_anodo_id]
-                pilha = ut.__init__(mat_catodo, mat_anodo, massa_catodo, massa_anodo)
-                break
-            elif (comando == "Show") or (comando == "show"):
-                show_json(dic)
-            elif (comando == "Sair") or (comando == "sair"):
-                break        
+            comando = input("Constrir sua própria pilha(C) ou Selecionar pilha comercial(S): ")
+            if (comando == "C") or (comando == "c"):
+                return 1
+            elif(comando == "S") or (comando == "s"):
+                return 0
             else:
-                print("Comando não encontrado")
-        
-        return pilha
+                print("comando inválido escolha novamente")
+                comando = input("Constrir sua própria pilha(C) ou Selecionar pilha comercial(S): ")
+def etapa1(dic):
+    print("--------------------------------------------------------------------------------------------")
+    print("ETAPA 1: SELEÇÃO DE MATERIAIS PARA A PILHA")
+    print("--------------------------------------------------------------------------------------------")
+    print("Esta é a lista de materiais disponíveis: ")
+    for element in dic:
+        print(dic[element]["id"]," -> ",dic[element]["nome"])
+    print("Para escolher o material você deve digitar o número que q aparece ao seu lado na lista apresentada a cima.")
+    mat1_id = 100
+    mat2_id = 100
+    while mat1_id > 10 or mat1_id < 1:
+        print("Não esqueça que temos apenas 10 materiais disponíeis.")
+        print("O id deve ser um número entre 1 e 10")
+        mat1_id = int(input("Qual o id do primeiro metal? "))
+    massa_mat1 = int(input("Qual é a massa (em g) do primeiro metal? "))
+    conc_sol_mat1 = int(input("Qual é a concentração da solução para primeiro metal em g/mol? "))
+    while mat2_id > 10 or mat2_id < 1:
+        print("Não esqueça que temos apenas 10 materiais disponíeis.")
+        print("O id deve ser um número entre 1 e 10")
+        mat2_id = int(input("Qual o id do metal do segundo metal? "))
+    massa_mat2 = int(input("Qual é a massa (em g) do metal do segundo metal? "))
+    conc_sol_mat2 = int(input("Qual é a concentração da solução para segundo metal em g/mol? "))
+    temp = int(input("Qual é a temperatura em graus celcius? "))
+    mat1 = dic[str(mat1_id)]
+    mat2 = dic[str(mat2_id)]
+    print(mat1)
+    print(mat2)
+    bateria = pilha(mat1, mat2, massa_mat1, massa_mat2,conc_sol_mat1,conc_sol_mat2, temp)
+    return bateria
 
-def etapa2(dic2, pilha): #Seleção de eletrólitos(soluções) e suas concentrações
+def etapa2(dic2):
         print("--------------------------------------------------------------------------------------------")
-        print("ETAPA 2: SELEÇÃO DE SOLUÇÕES E SUAS CONCENTRAÇÕES")
-        print("Comandos:")
-        print("Show             -------------------       Mostra as soluções disponíveis")
-        print("Selecionar       -------------------       Selecione o número correspondente à solução")
-        print("Sair")
+        print("ETAPA 2: SELEÇÃO DE PILHA COMERCIAL PARA PARÂMETROS DADOS")
         print("--------------------------------------------------------------------------------------------")
+        ddp_usuario = int(input("Qual a DDP da pilha que você precisa, em V? "))
+        pot_usuario = int(input("Qual a potência da pilha que você precisa, em W/h? "))
+        tempo_usuario = int(input("Quanto tempo a pilha precisa ficar ligada, em horas? "))
+        cap_carga_usuario = int(input("Qual a capacidade de carga da pilha que você precisa, em Ah? "))
+        limite_usuario = int(input("Caso não seja possível conseguir a ddp exata, qual o erro que poderemos aceitar, em V? "))
+        
+        bateria_usu = Bateria_Escolhida(ddp_usuario,pot_usuario,tempo_usuario,cap_carga_usuario, limite_usuario)
+        escolhida, total, em_paralelo, em_serie = bateria_usu.escolha(dic2)
+        return escolhida, total, em_paralelo, em_serie
 
 
 def __init__():
-    comando = 0
-    while(True):
-        with open('data.json', 'r') as fp:
-            dic1 = json.load(fp)
+    sair = 0
+    with open('data.json', 'r') as fp:
+        dic1 = json.load(fp)
+    with open('comerciais.json', 'r') as cm:
+        dic2 = json.load(cm)
+    while(sair == 0):
+        resposta = menu_principal(dic1)
+        if resposta == 1:
+            bateria = etapa1(dic1)
+            print("--------------------------------------------------------------------------------------------")
+            print("A DDP dessa bateria em V será:")
+            print(bateria.ddp)
+            print("A Capacidade de carga dessa bateria, em mAh será:")
+            print(bateria.cap_carga)
+            print("A Potência dessa bateria em W/h será:")
+            print(bateria.potencia)
+            print("--------------------------------------------------------------------------------------------")
+        if resposta == 0:
+            bateria, total, em_paralelo, em_serie = etapa2(dic2)
+            print(bateria)
+            print("--------------------------------------------------------------------------------------------")
+            print("Você pode usar a pilha", bateria["nome"])
+            print("serão",em_serie," pilhas em série repetidas ",em_paralelo," vezes em paralelo")
+            print("Completando um total de ",total," pilhas")
+            print("cada pilha custa ",bateria["preco"]," reais, então você gastará ",bateria["preco"]*total," reais no total")
+            print("--------------------------------------------------------------------------------------------")
+        
+        sair = int(input("Para sair digite 1, para voltar para o início digite 0: "))
 
-        pilha = etapa1(dic1)
 
 
 __init__()
--- utils.py
+++ utils.py
@@ -1,21 +1,23 @@
+from math import *
+
 class pilha():
-    def __init__(self,mat1,mat2,massa1,massa2):
+    def __init__(self,mat1,mat2,massa1,massa2,con_sol1,con_sol2,temp):
         self.mat1 = mat1
         self.mat2 = mat2
-        self.ddp = calcula_DDP(mat1,mat2)
+        self.ddp = calcula_DDP(mat1,mat2,con_sol1,con_sol2,temp)
         self.cap_carga = calcula_cap(mat1,mat2,massa1,massa2)
-        self.potencia = calcula_potencia(mat1,mat2)
+        self.potencia = calcula_potencia(self,mat1,mat2)
         self.tempo_ligado = calcula_tempo(mat1,mat2)
 
-def calcula_DDP(mat1,mat2):
-    maior = mat1.E
+def calcula_DDP0(mat1,mat2,con_sol1,con_sol2):
+    maior = mat1["E"]
 
-    if (mat2.E > maior):
-        ddp = mat2.E - maior
-        return ddp
+    if (mat2["E"] > maior):
+        E0 = mat2["E"] - maior
+        return E0, mat2, mat1, con_sol2, con_sol1
     
-    ddp = maior - mat2.E
-    return ddp
+    E0 = maior - mat2["E"]
+    return E0, mat1, mat2, con_sol1, con_sol2
 
 def calcula_cap(mat1,mat2,massa1,massa2):
     c_eletron = 1.6021 * (10 ** (-19))
@@ -30,14 +32,90 @@ def calcula_cap(mat1,mat2,massa1,massa2):
         material = mat2
         contra_mat = mat1
 
-    total_mol_e = (material["eletron"] * limitante * contra_mat["eletron"]) / (material["massa"])
+    total_mol_e = (material["eletrons"] * limitante * contra_mat["eletrons"]) / (material["M"])
 
     Ah = F * total_mol_e / 3600
-    #print(Ah)
     return Ah
 
-def calcula_potencia():
-    pass
+def calcula_DDP(mat1,mat2,con_sol1,con_sol2,temp):
+    R = 8.314
+    T = temp + 273.15
+    F =  96485
+    E0, cat, ano, con_cat, con_ano = calcula_DDP0(mat1,mat2,con_sol1, con_sol2)
+    n = mat1["eletrons"]*mat2["eletrons"]
+    K = (con_ano**ano["eletrons"])/(con_cat**cat["eletrons"])
+    ddp = E0 - ((R*T)/(n*F))*log(K)
+    return ddp
 
-def calcula_tempo():
-    pass
+def calcula_potencia(self,mat1,mat2):
+    pot = self.ddp * self.cap_carga
+    return pot/1000
+
+def calcula_tempo(mat1,mat2): # não parece necessário - ela apenas pede o tempo para a segunda parte
+    return 1
+
+class Bateria_Escolhida():
+    def __init__(self, ddp_usuario, pot_usuario, tempo_usuario, cap_carga_usuario, limite):
+        self.ddp = ddp_usuario
+        self.pot = pot_usuario
+        self.tempo = tempo_usuario
+        self.cap_carga = cap_carga_usuario
+        self.limite = limite
+    
+    def escolha(self,dic):
+        quantP = 1
+        quantS = 1
+        quantT = 1
+        # for item in dic:
+        #     print(item["ddp"])
+        #     if item["ddp"] < 3:
+        #         if abs(item["ddp"] - self.ddp) < 1:
+        #             quantP = paralelo(self.pot,self.tempo,item)
+        #             quantS = serie(self.ddp, item)
+        #             quantT = quantP*quantS
+        #             return item, quantT, quantP, quantS #devolve a pilha, a quantidade total, a quantidade em paralelo e a quantidade em série
+        #     else:
+        #         if abs(item["ddp"] - self.ddp) <= 3:
+        #             quantP = paralelo(self.pot,self.tempo,item)
+        #             quantS = serie(self.ddp, item)
+        #             quantT = quantP*quantS
+        #             return item, quantT, quantP, quantS
+        #         if self.ddp > 15:
+        #             quantP = paralelo(self.pot,self.tempo,item)
+        #             quantS = serie(self.ddp, item)
+        #             quantT = quantP*quantS
+        #             return item, quantT, quantP, quantS
+
+        for item in dic:
+            lista_preco = []
+            print(dic[item], "itemmmmmmmmmmmmmmmmmmmmmm")
+            quantP = paralelo(self.pot,self.tempo,dic[item])
+            quantS = serie(self.ddp, dic[item])
+            quantT = quantP*quantS
+            preco = quantT*dic[item]["preco"]
+            lista_preco.append(preco)
+            minimo = min(lista_preco)
+            index_min = lista_preco.index(minimo)
+            print(dic[str(index_min+1)])
+            print("------------------------------------------------")
+        return dic[str(index_min)], quantT, quantP, quantS
+
+def paralelo(pot_usuario,tempo_usuario,item):
+    i = pot_usuario/item["ddp"]
+    tempo = item["cap_carga"]/i # tempo que ficará ligada
+    if tempo > tempo_usuario: # se o tempo obtido é maior que o esperado tudo OK
+        quant = 1 # quantidade de pilhas
+        return quant
+    else: # se o tempo obtido é menor que o esperado
+        quant = tempo_usuario/tempo #verifica quantas pilhas em paralelo serão necessárias
+        quant = int(quant)+1 # não devolve números quebrados e sempre arredonda para cima
+        return quant #devolve a quantidade de paralelos
+
+def serie(ddp_usuario,item):
+    if(item["ddp"] >= ddp_usuario):
+        return 1
+    else:
+        quant = int(ddp_usuario/item["ddp"])+1
+        return quant
+
+
